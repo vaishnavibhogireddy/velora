d52f8578f29c94624862db1129a511afad48c6b6
vaishnavi <bhogireddyvaishnavi@gmail.com>
2026-07-05 21:26:54 +0530

    Setup Express backend and connect MongoDB Atlas

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Velora Backend 🚀");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
